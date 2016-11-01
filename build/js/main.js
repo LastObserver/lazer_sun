6c2484f15d3a7c815a64a940f62ed783495bbdbf
Your Name <you@example.com>
2016-11-01 14:07:08 +0500

    homework

---
+++ build/js/main.js
@@ -0,0 +1,1 @@
+alert('PRAISE THE SUN!!! \O/');
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var pug = require('gulp-pug');
 
+var scss = require('gulp-sass');
+
 var gulp = require('gulp');
 
 var browserSync = require('browser-sync');
@@ -10,16 +12,19 @@ var watch = require('gulp-watch')
 
 var path = {
 	src: {
-		pug: './src/html/*.pug'
-		scss: './src/css/*.scss'
+		pug: './src/html/*.pug',
+		scss: './src/css/*.scss',
+		js: './src/js/*.js'
 	},
 	build: {
-		html: './build/html/'
-		css: './build/css/'
+		html: './build/html/',
+		css: './build/css/',
+		js: './build/js/'
 	},
 	watch: {
-		pug: './src/html/*.pug'
-		scss: './src/css/*.scss'
+		pug: './src/html/*.pug',
+		scss: './src/css/*.scss',
+		js: './src/js/*.js'
 	}
 };
 
@@ -41,12 +46,18 @@ gulp.task('css',function(){
 		.pipe(scss({pretty: true}))
 		.pipe(gulp.dest(path.build.css))
 		.on('end',browserSync.reload)
-})
+});
+
+gulp.task('js',function(){
+	gulp.src(path.src.js)
+		.pipe(gulp.dest(path.build.js))
+		.on('end',browserSync.reload)
+});
 
 gulp.task('webserver',function(){
 	browserSync({
 		server: {
-			baseDir: "./build/html/"
+			baseDir: "./build/"
 		}
 	});
 });
@@ -55,9 +66,12 @@ gulp.task('watch',function(){
 	watch([path.watch.pug], function(event,cb){
 		gulp.start('html');
 	});
-	watch([path.watch.scss],function(event.cb){
+	watch([path.watch.scss],function(event,cb){
 		gulp.start('css');
+	});
+	watch([path.watch.js],function(event,cb){
+		gulp.start('js');
 	});
 });
 
-gulp.task('default',['html','watch','webserver']);
+gulp.task('default',['html','css','js','watch','webserver'])
